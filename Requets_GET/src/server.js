911ca41eb2e51fc08c4a0ee1bf7c142836fdfc1f
VATSU_04 <vatsumalambo@gmail.com>
2025-12-18 13:14:31 +0200

    refactor: Move `index.html` to `public` directory and configure server to serve static assets via `express.static`.

--- Requets_GET/src/server.js
+++ Requets_GET/src/server.js
@@ -5,11 +5,12 @@ const app = express();
 const ESP_IP = '10.42.0.77'; // IP ESP32
 const PORT = 3000;
 
-// app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//   res.sendFile('../public/index.html');
+// });
 
+console.log(__dirname)
 
 app.get('/control/:pin/:state', async (req, res) => {
   const { pin, state } = req.params;
