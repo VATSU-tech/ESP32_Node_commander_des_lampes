ed99be79d2f0be1182f0ff0d751469fac72cf3ce
VATSU_04 <vatsumalambo@gmail.com>
2025-12-18 20:00:14 +0200

    feat: move `index.html` to root and update ESP32 WiFi credentials with connection status indicator.

--- Requets_GET_online/ESP_code/esp32_led.ino
+++ Requets_GET_online/ESP_code/esp32_led.ino
@@ -1,13 +1,15 @@
-
-#include <WiFi.h>
+//VATSU-tech
+#include <WiFi.h> // Use #include <ESP8266WiFi.h> for ESP8266 models
 #include <HTTPClient.h>
 
-const char* ssid = "YOUR_WIFI";
-const char* password = "YOUR_PASSWORD";
+const char* ssid = "Heritiervita";
+const char* password = "0987654321"; 
 
 String serverUrl = "https://esp32-node-commander-des-lampes-1.onrender.com/";
 
 int ledPin = 2;
+
+bool stateL = true;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -15,8 +17,13 @@ void setup() {
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(ledPin, stateL);
+    Serial.print(".");
+    stateL =! stateL;
     delay(500);
   }
+  Serial.print("Connexion : "+WiFi.localIP());
+  digitalWrite(ledPin, false);
 }
 
 void loop() {
